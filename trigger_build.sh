5cf405fac8fcef9e7725abf107adabc866527f81
Koen Schmeets <k.schmeets@gmail.com>
2016-06-18 05:23:49 +0200

    Updating log

--- trigger_build.sh
+++ trigger_build.sh
@@ -13,6 +13,7 @@ git push origin master
 echo "Waiting for log to complete, hit enter when done."
 read _
 ./log.sh > .log
+docker-compose logs > .compose-log
 git add .
 git commit -am "Updating log"
 git push origin master
