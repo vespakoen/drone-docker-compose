bde94476e3f8effa10969bf0e70239e0a98dd756
Koen Schmeets <k.schmeets@gmail.com>
2016-06-18 04:56:54 +0200

    Trigger build #29

--- trigger_build.sh
+++ trigger_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo $(expr $(cat .build) + 1) > .build
-./last_logs.sh > .log
+./log.sh > .log
 git add .
 git commit -am "Trigger build #$(cat .build)"
 git push --set-upstream origin master
