b9e5e19780bf011bf78160fa7713144197298bcd
Koen Schmeets <k.schmeets@gmail.com>
2016-06-18 04:42:06 +0200

    Trigger build #26

--- last_logs.sh
+++ last_logs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sqlite3 /drone/drone.sqlite "select log_data from logs order by log_id desc limit 1;" | jq .out
+sqlite3 /drone/drone.sqlite "select log_data from logs order by log_id desc limit 1;" | jq --raw-output .out
