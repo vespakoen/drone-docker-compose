e26f8e517897ec3772d72f71c0f2f84695f7cd29
Koen Schmeets <k.schmeets@gmail.com>
2016-06-18 19:16:44 +0200

    Updating log

--- add_secrets.sh
+++ add_secrets.sh
@@ -10,9 +10,11 @@ function rm_secret {
   ./release/linux/amd64/drone -t $DRONE_USER_TOKEN -s http://localhost:8000 secret rm vespakoen/drone-docker-compose $1
 }
 
+rm_secret DOCKER_EMAIL
 rm_secret DOCKER_REGISTRY
 rm_secret DOCKER_USER
 rm_secret DOCKER_PASSWORD
+add_secret DOCKER_EMAIL $DOCKER_REGISTRY_EMAIL
 add_secret DOCKER_REGISTRY $DOCKER_REGISTRY_URL
 add_secret DOCKER_USER $DOCKER_REGISTRY_USER
 add_secret DOCKER_PASSWORD $DOCKER_REGISTRY_PASSWORD
