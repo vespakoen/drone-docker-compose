472c242e3377506a7196d0a09ab9631179e8b6c0
Koen Schmeets <k.schmeets@gmail.com>
2016-06-18 19:31:07 +0200

    Updating drone submodule, removing environment variables from docker plugin, make rebuild optional in up.sh

--- up.sh
+++ up.sh
@@ -1,7 +1,12 @@
 . ./env.sh
-./recompiledrone.sh
-docker-compose stop
-docker-compose build
+
+if [ "$1" == "--rebuild" ]; then
+  ./recompiledrone.sh
+  docker-compose stop
+  docker-compose build
+else
+  docker-compose stop
+fi
 docker-compose up -d
 sleep 8
 ./add_secrets.sh
