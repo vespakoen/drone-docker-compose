e4e822be17f07c7b305cd5cea35676f9517fc64e
Koen Schmeets <k.schmeets@gmail.com>
2016-06-18 17:12:30 +0200

    Without privileged plugins again

--- add_secrets.sh
+++ add_secrets.sh
@@ -3,5 +3,5 @@
 . ./env.sh
 
 ./release/linux/amd64/drone -t $DRONE_USER_TOKEN -s http://localhost:8000 secret add --image=docker vespakoen/drone-docker-compose DOCKER_REGISTRY $DOCKER_REGISTRY_URL
-./release/linux/amd64/drone -t $DRONE_USER_TOKEN -s http://localhost:8000 secret add --image=docker vespakoen/drone-docker-compose DOCKER_USER $DOCKER_REGISTRY_USER
-./release/linux/amd64/drone -t $DRONE_USER_TOKEN -s http://localhost:8000 secret add --image=docker vespakoen/drone-docker-compose DOCKER_PASSWORD $DOCKER_REGISTRY_PASSWORD
+# ./release/linux/amd64/drone -t $DRONE_USER_TOKEN -s http://localhost:8000 secret add --image=docker vespakoen/drone-docker-compose DOCKER_USER $DOCKER_REGISTRY_USER
+# ./release/linux/amd64/drone -t $DRONE_USER_TOKEN -s http://localhost:8000 secret add --image=docker vespakoen/drone-docker-compose DOCKER_PASSWORD $DOCKER_REGISTRY_PASSWORD
