b9b75d373b663550de5b09f4682cbeaf7a80084a
Koen Schmeets <k.schmeets@gmail.com>
2016-06-20 13:44:46 +0200

    Fix sign_yaml.sh

--- sign_yaml.sh
+++ sign_yaml.sh
@@ -3,4 +3,4 @@
 . ./env.sh
 
 echo "Signing .drone.yml"
-docker run -v $(pwd):/project --rm drone/drone:0.5 -t $DRONE_USER_TOKEN -s http://localhost:8000 sign vespakoen/drone-docker-compose --in "/project/.drone.yml" --out "/project/.drone.yml.sig"
+docker run --net dronedockercompose_default -v $(pwd):/project --rm drone/drone:0.5 -t $DRONE_USER_TOKEN -s http://drone:8000 sign vespakoen/drone-docker-compose --in "/project/.drone.yml" --out "/project/.drone.yml.sig"
